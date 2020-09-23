75a327c49b07d312db1c170a4aa8d44b3262b5be
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2020-09-23 16:29:39 +0200

    refactor: factorize view model

--- ClientConvertisseurV2/ClientConvertisseurV2/ViewModel/MainViewModel.cs
+++ ClientConvertisseurV2/ClientConvertisseurV2/ViewModel/MainViewModel.cs
@@ -1,104 +1,13 @@
-﻿using ClientConvertisseurV2.Models;
-using ClientConvertisseurV2.Service;
-using GalaSoft.MvvmLight;
-using GalaSoft.MvvmLight.Command;
-using System;
-using System.Collections.ObjectModel;
-using System.Windows.Input;
-using Windows.UI.Popups;
+﻿using ClientConvertisseurV2.Service;
+using System.Threading.Tasks;
 
 namespace ClientConvertisseurV2.ViewModel
 {
-    public class MainViewModel : ViewModelBase
+    public class MainViewModel : ConverterViewModel
     {
-        private ObservableCollection<Currency> _comboBoxCurrencies;
-
-        public ObservableCollection<Currency> ComboBoxCurrencies
+        protected override async Task<double> DoConversion(int currencyId, double amount)
         {
-            get { return _comboBoxCurrencies; }
-            set
-            {
-                _comboBoxCurrencies = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        private string _amountFrom;
-
-        public string AmountFrom
-        {
-            get { return _amountFrom; }
-            set
-            {
-                _amountFrom = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        private Currency currency;
-
-        public Currency SelectedCurrency
-        {
-            get { return currency; }
-            set
-            {
-                currency = value;
-                RaisePropertyChanged();
-            }
-        }
-
-        private double _amountTo;
-
-        public double AmountTo
-        {
-            get { return _amountTo; }
-            set
-            {
-                _amountTo = value;
-                RaisePropertyChanged();
-            }
-        }
-
-
-
-        public ICommand BtnSetConversion { get; private set; }
-
-        public MainViewModel()
-        {
-            ActionGetData();
-            BtnSetConversion = new RelayCommand(ActionSetConversion);
-        }
-
-        private async void ActionGetData()
-        {
-            var result = await WSService.GetAllCurrenciesAsync();
-            ComboBoxCurrencies = new ObservableCollection<Currency>(result);
-        }
-
-        private async void ActionSetConversion()
-        {
-            if (AmountFrom.Length == 0)
-            {
-                var dialog = new MessageDialog("Please enter an amount to convert");
-                await dialog.ShowAsync();
-                return;
-            }
-            if (SelectedCurrency == null)
-            {
-                var dialog = new MessageDialog("Please select a currency");
-                await dialog.ShowAsync();
-                return;
-            }
-            try
-            {
-                var amountFrom = Convert.ToDouble(AmountFrom);
-                AmountTo = await CurrencyService.ConvertCurrency(SelectedCurrency.Id, amountFrom);
-            }
-            catch (Exception ex)
-            {
-                var dialog = new MessageDialog(ex.Message);
-                await dialog.ShowAsync();
-            }
+            return await CurrencyService.ConvertCurrency(currencyId, amount);
         }
     }
 }
