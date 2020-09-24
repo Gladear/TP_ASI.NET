1ee68ce526c298efa3fa0b3e559cb749ecaa038d
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2020-09-24 14:59:31 +0200

    feat: test selecting data

--- TP2Console/TP2Console/Program.cs
+++ TP2Console/TP2Console/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Npgsql.TypeHandlers.GeometricHandlers;
 using System;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using TP2Console.Models.EntityFramework;
 
 namespace TP2Console
@@ -9,18 +11,77 @@ namespace TP2Console
     {
         static void Main(string[] args)
         {
-            using (var ctx = new FilmsDBContext())
+            var ctx = new FilmsDBContext();
+            //Exo2Q1(ctx);
+            //Exo2Q2(ctx);
+            //Exo2Q3(ctx);
+            //Exo2Q4(ctx);
+            //Exo2Q5(ctx);
+            //Exo2Q6(ctx);
+            //Exo2Q7(ctx);
+            //Exo2Q8(ctx);
+            Exo2Q9(ctx);
+            Console.ReadKey();
+        }
+
+        public static void Exo2Q1(FilmsDBContext ctx)
+        {
+            foreach (var film in ctx.Film)
             {
-                //Chargement de la catégorie Action
-                Categorie categorieAction = ctx.Categorie.First(c => c.Nom == "Action");
-                Console.WriteLine("Categorie : " + categorieAction);
-                Console.WriteLine("Films : ");
-                //Chargement des films de la catégorie Action.
-                foreach (var film in categorieAction.Film) // lazy loading initiated
-                {
-                    Console.WriteLine(film);
-                }
+                Console.WriteLine(film.ToString());
             }
+        }
+
+        public static void Exo2Q2(FilmsDBContext ctx)
+        {
+            foreach (var u in ctx.Utilisateur)
+            {
+                Console.WriteLine(u.Email);
+            }
+        }
+
+        public static void Exo2Q3(FilmsDBContext ctx)
+        {
+            foreach (var u in ctx.Utilisateur.OrderBy(u => u.Login))
+            {
+                Console.WriteLine(u);
+            }
+        }
+
+        public static void Exo2Q4(FilmsDBContext ctx)
+        {
+            foreach (var f in ctx.Film.Where(f => f.CategorieNavigation.Nom == "Action"))
+            {
+                Console.WriteLine(f.Id + " : " + f.Nom);
+            }
+        }
+
+        public static void Exo2Q5(FilmsDBContext ctx)
+        {
+            Console.WriteLine(ctx.Categorie.Count());
+        }
+
+        public static void Exo2Q6(FilmsDBContext ctx)
+        {
+            Console.WriteLine(ctx.Avis.Min(a => a.Note));
+        }
+
+        public static void Exo2Q7(FilmsDBContext ctx)
+        {
+            foreach (var f in ctx.Film.Where(f => f.Nom.ToUpper().StartsWith("LE")))
+            {
+                Console.WriteLine(f);
+            }
+        }
+
+        public static void Exo2Q8(FilmsDBContext ctx)
+        {
+            Console.WriteLine(ctx.Avis.Where(a => a.FilmNavigation.Nom.ToUpper().Equals("PULP FICTION")).Average(a => a.Note));
+        }
+
+        public static void Exo2Q9(FilmsDBContext ctx)
+        {
+            Console.WriteLine(ctx.Avis.OrderByDescending(a => a.Note).First().UtilisateurNavigation);
         }
     }
 }
